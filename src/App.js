d1c9676959907d6b8c6b00450b0a8e1f6506cf68
Omar Suriel <omarsuriel@icloud.com>
2021-07-28 11:59:02 -0400

    init again

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import { Provider, useDispatch } from 'react-redux'
+import { Provider } from 'react-redux'
 
-import { createStore, compose } from 'redux'
-import { useSelector } from 'react-redux';
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-
-const reducer = (state, action) => {
-
-  if (action.type === 'increase') return state + 1
-  if (action.type === 'decrease') return state - 1
-
-  return state
-}
-
-const store = createStore(
-  reducer,
-  0,
-  composeEnhancers(),
-  )
 
 function App() {
-  const dispatch = useDispatch();
-  const counter = useSelector((state) => state)
-
-  const increase = () => dispatch({type: 'increase'})
-  const decrease = () => dispatch({type: 'decrease'})
-
-
   return (
-    <Provider store={store}>
+    <Provider>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -46,15 +20,11 @@ function App() {
           >
           Learn React
         </a>
-        <button onClick={increase}>increase</button>
-        <button onClick={decrease}>decrease</button>
+        <button onClick={}>increase</button>
+        <button onClick={}>decrease</button>
         <h1>{counter}</h1>
       </header>
     </div>
     </Provider>
   );
 }
-
-export default () => <Provider store={store}>
-  <App />
-</Provider>
