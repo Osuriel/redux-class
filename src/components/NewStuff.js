51b13ff8954916d8bcdee84d5550ad784f48b785
Omar Suriel <omarsuriel@icloud.com>
2021-08-12 21:48:51 -0400

    redux connect function

--- src/components/NewStuff.js
+++ src/components/NewStuff.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import { Component } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, connect } from 'react-redux';
 import colorProviderHOC from '../hoc/colorProviderHOC';
 import quoteProviderHOC from '../hoc/quoteProviderHOC';
 
-class NewStuff extends Component {
-  // ITs huge with lots of code.
-  // ITs huge with lots of code.
-  // ITs huge with lots of code.
-  // ITs huge with lots of code.
-  // ITs huge with lots of code.
-  // ITs huge with lots of code.
-  // ITs huge with lots of code.
-  
-  
+class NewStuff extends Component {  
   render(){
     console.log('newStuff props: ', this.props)
     const { quote } = this.props;
     const color = 'blue';
+    this.props.dispatch({type: 'random just testing'})
     return (
       <div style={{ padding: '30px 0', border: `1px solid ${color}` }}>
         <h2>new stuff</h2>
@@ -28,6 +20,17 @@ class NewStuff extends Component {
   }
 }
 
+const mapStateToProps = (state) => {
+  return {
+    chicken: "chick fil a",
+    random:"text",
+    quote: state.shoppingCart.quote,
+  }
+};
 
+// // 1. we get a HOC
+// const reduxHOC = connect(mapStateToProps);
+// // 2. we use that HOC
+// export default reduxHOC(NewStuff);
 
-export default quoteProviderHOC(NewStuff);
+export default connect(mapStateToProps)(NewStuff);
--- src/hoc/quoteProviderHOC.js
+++ src/hoc/quoteProviderHOC.js
@@ -3,9 +3,9 @@ import { useSelector } from 'react-redux';
 
 const quoteProviderHOC = (Component) => {
 
-  return () => {
+  return (props) => {
     const quote = useSelector(state => state.shoppingCart.quote)
-    return <Component quote={quote} />
+    return <Component quote={quote} {...props} />
   }
 }
 
